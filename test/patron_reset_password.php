6358a8e06338f9fac75c8bc5420a052c696307f6
John Houser <90853393+john-c-houser@users.noreply.github.com>
2022-07-25 09:51:40 -0700

    Better duplicate checking

--- test/patron_reset_password.php
+++ test/patron_reset_password.php
@@ -14,7 +14,7 @@ print "Connecting\n\n";
 $token = $ilsws->connect();
 
 print "patron_reset_password\n\n";
-$url = 'https://multcolib.org?token=<RESET_PASSWORD_TOKEN>';
+$url = 'https://multcolib.org/reset-my-pin-callback';
 $response = $ilsws->patron_reset_password($token, $barcode, $url, $email);
 $json = json_encode($response, JSON_PRETTY_PRINT);
 print "$json\n\n";
